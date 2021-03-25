97dd182aa5ebba44d9b8b1007abe71e6780a9045
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-25 15:07:48 +0000

    Unit7-06: Loop Statements

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Variables I will need for Loop
-let number = 15
+let num = 15
 let guess = 0
 
 // This is used for my loop for 'guess my age'
-while (guess != number) {
-  guess = prompt ('Guess a number between 1 and 20')
+while (guess !== num) {
+  guess = prompt('Guess a number between 1 and 20')
   guess = parseInt(guess)
-  if (guess > number) {
+  if (guess > num) {
     alert('Your Guess is High! Try Again!')
-  } else if (guess < number) {
+  } else if (guess < num) {
     alert('Your Guess is Low! Try Again!')
-  } if (number === guess) {
+  } if (num === guess) {
     alert('You WIN!!')
-  } 
+  }
   }
